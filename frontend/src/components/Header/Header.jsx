d35cf428d3ef4b4d18cc3e6f452bbbe7aefa393e
RAHUL <dasr16583@gmail.com>
2023-07-28 21:24:44 +0530

    sucessfully creating moon and sun ushing three js

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 import { ReactNavbar } from "overlay-navbar";
+import Logo from "../../images/logo.png";
+import { FaUserAlt} from "react-icons/fa";
 
 const Header = () => {
   return (
     <ReactNavbar
-        navColor1="white"
-        navColor2="hsl(219,48%,8%)"
-        burgerColor="hsl(250,100%,75%)"
-        burgerColorHover="hsl(250,100%,75%)"
-        // logo={logo}
-        logoWidth="250px"
-        logoHoverColor="hsl(250,100%,75%)"
-        nav2justifyContent="space-around"
-        nav3justifyContent="space-around"
-        link1Text="Home"
-        link2Text="About"
-        link3Text="Projects"
-        link4Text="Contact"
-        link1Url='/'
-        link2Url='/about'
-        link3Url="/projects"
-        link4Url="/contact"
-        linkColorHover="white"
-        link1Color="hsl(250,100%,75%)"
-        link1Size="1.5rem"
-        link1Padding="3vmax"
-        />
-    );
+      navColor1="white"
+      navColor2="hsl(219,48%,8%)"
+      burgerColor="hsl(250,100%,75%)"
+      burgerColorHover="hsl(250,100%,75%)"
+      logo={Logo}
+      logoWidth="250px"
+      logoHoverColor="red"
+      nav2justifyContent="space-around"
+      nav3justifyContent="space-around"
+      link1Text="Home"
+      link2Text="About"
+      link3Text="Projects"
+      link4Text="Contact"
+      link1Url="/"
+      link2Url="/about"
+      link3Url="/projects"
+      link4Url="/contact"
+      link1ColorHover="white"
+      link1Color="hsl(250,100%,75%)"
+      link1Size="1.5rem"
+      link1Padding="3vmax"
+      profileIcon={true}
+      ProfileIconElement={FaUserAlt}
+      profileIconColor="hsl(250, 100%, 75%)"
+      profileIconColorHover="white"
+    />
+  );
 };
 
 export default Header;
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,11 +1,107 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import * as THREE from "three";
+import "./Home.css";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import moonImage from "../../images/moon.jpg";
+import venusImage from "../../images/venus.jpg";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  useEffect(() => {
+    const textureLoader = new THREE.TextureLoader();
+    const moonTexture = textureLoader.load(moonImage);
+    const venusTexture = textureLoader.load(venusImage);
 
-export default Home
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      100
+    );
+    camera.position.set(4, 4, 4);
+
+    const canvas = document.querySelector(".homeCanvas");
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
+    // creating moon
+    const moonGeometry = new THREE.SphereGeometry(2, 64, 64);
+    const moonMaterial = new THREE.MeshStandardMaterial({ map: moonTexture });
+    const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+
+    // cretaing venus
+    const venusGeometry = new THREE.SphereGeometry(3, 64, 64);
+    const venusMaterial = new THREE.MeshBasicMaterial({ map: venusTexture });
+    const venus = new THREE.Mesh(venusGeometry, venusMaterial);
+
+    venus.position.set(8, 5, 5);
+
+    const pointLight = new THREE.PointLight(0xffffff, 1);
+    const pointLight2 = new THREE.PointLight(0xffffff, 0.1);
+    pointLight.position.set(8, 5, 5);
+    pointLight2.position.set(-8, -5, -5);
+
+    // const controls = new OrbitControls(camera, renderer.domElement);
+    scene.add(moon);
+    scene.add(venus);
+    scene.add(pointLight);
+    scene.add(pointLight2);
+
+    const constSpeed = 0.01;
+
+    window.addEventListener("mousemove", (e) => {
+      if (e.clientX <= window.innerWidth / 2) {
+        moon.rotation.x += constSpeed;
+        moon.rotation.y -= constSpeed;
+
+        venus.rotation.x += constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+
+      if (e.clientX > window.innerWidth / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y -= constSpeed;
+
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+
+      if (e.clientY > window.innerHeight / 2) {
+        moon.rotation.x += constSpeed;
+        moon.rotation.y -= constSpeed;
+
+        venus.rotation.x += constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+
+      
+      if (e.clientY <= window.innerHeight / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y -= constSpeed;
+
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+    });
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      moon.rotation.y += 0.01;
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.render(scene, camera);
+  }, []);
+
+  return (
+    <div className="home">
+      <canvas className="homeCanvas"></canvas>
+    </div>
+  );
+};
+
+export default Home;
