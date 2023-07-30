9e38e4e729cdeaf445edf2d330553c76bc198079
RAHUL <dasr16583@gmail.com>
2023-07-30 22:39:03 +0530

    working done upto about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-
-import './App.css';
-import {BrowserRouter as Router, Route,Routes} from "react-router-dom"
-import Home from './components/Home/Home';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import About from "./components/About/About";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-      <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </Router>
