f2b781f9072fe2f21de0386c310a7a3faa805c11
RAHUL <dasr16583@gmail.com>
2023-07-31 20:48:09 +0530

    working done of projects page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
     </Router>
