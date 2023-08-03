6cfa3b027901efefd721ac6c53fd7c4172a000a8
RAHUL <dasr16583@gmail.com>
2023-08-03 16:27:16 +0530

    working on database mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,22 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 import mongoose from "mongoose";
 
+const PORT = process.env.PORT || 4000;
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
-console.log("MONGO_URI:", process.env.MONGO_URI); // Add this line for debugging
+// connection of mongodb
+ mongoose
+    .connect("mongodb://127.0.0.1:27017/protfolio", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err.message);
+    });
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
-
-const MONGO_URI = process.env.MONGO_URI;
-console.log("MONGO_URI:", MONGO_URI); // Add this line for debugging
-
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err.message);
-  });
