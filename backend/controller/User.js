324b8a17540fc61844b67283d2e60e9eca5ba355
RAHUL <dasr16583@gmail.com>
2023-08-09 11:30:10 +0530

    solving some api issues

--- backend/controller/User.js
+++ backend/controller/User.js
@@ -1,12 +1,13 @@
 import { sendMail } from "../middlewares/sendmail.js";
 import { User } from "../model/User.js";
-import Jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import cloudinary from "cloudinary";
 
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email, password });
+
     if (!user) {
       return res.status(400).json({
         success: false,
@@ -14,8 +15,8 @@ export const login = async (req, res) => {
       });
     }
 
-    const JWT_SECRET = process.env.JWT_SECRET;
-    const token = Jwt.sign({ _id: user._id }, JWT_SECRET);
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    console.log(process.env.JWT_SECRET)
 
     res
       .status(200)
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
-import dotenv from "dotenv";
 import { app } from "./app.js";
 import mongoose from "mongoose";
 import cloudinary from "cloudinary";
+import dotenv from "dotenv";
 
-const PORT = process.env.PORT || 4000;
 dotenv.config();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
 
 // connection of mongodb
 mongoose
-  .connect("mongodb://127.0.0.1:27017/protfolio", {
+  .connect(`mongodb://${process.env.MONGO_URI}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -18,10 +22,6 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err.message);
   });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
-});
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
