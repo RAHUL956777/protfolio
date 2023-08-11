758df1c58a86cfecb7643e32fe882201812be4fe
RAHUL <dasr16583@gmail.com>
2023-08-11 17:20:55 +0530

    change the protfolio

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import {
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+} from "./canvas";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Tech from "./Tech";
+import Works from "./Works";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+};
